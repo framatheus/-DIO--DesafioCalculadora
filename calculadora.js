bc1c5590bb926004af9f35c3875366242ae88158
Matheus Franchin <99729452+framatheus@users.noreply.github.com>
2024-01-09 13:39:23 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+let vitorias = parseInt(prompt("Digite a quantidade de vitorias: "));
+let derrotas = parseInt(prompt("Digite a quantidade de derrotas: "));
+let saldo = vitorias -derrotas;
+let nivel;
+
+if(vitorias <10) {
+    nivel = "Ferro";
+} else if (vitorias >=11 && vitorias <=20) {
+    nivel = "Bronze";
+} else if (vitorias >=21 && vitorias <=50) {
+    nivel = "Prata";
+} else if (vitorias >=51 && vitorias <=80) {
+    nivel = "Ouro";
+} else if (vitorias >=81 && vitorias <=90) {
+    nivel = "Diamante";
+} else if (vitorias >=91 && vitorias <=100) {
+    nivel = "Lendário";
+} else {
+    nivel = "Imoral";
+}
+
+alert(`O Herói tem um saldo de ${saldo} e está no nível de ${nivel}`)
